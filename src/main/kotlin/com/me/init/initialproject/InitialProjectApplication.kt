810259c497b3e59a7e5c03a3c71c781570463aa4
injinjeong <snowwwwww@naver.com>
2023-03-30 14:07:03 +0900

    first commit

---
+++ src/main/kotlin/com/me/init/initialproject/InitialProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.me.init.initialproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InitialProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<InitialProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/me/init/initialproject/InitialProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.me.init.initialproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InitialProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
